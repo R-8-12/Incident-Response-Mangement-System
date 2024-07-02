e1a8b6c4d77f27ff4bf54daa271e7d366bde4408
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2024-07-02 15:24:28 +0530

    5th commti Successfully works for sender and receivers' mail transfer.

--- app.py
+++ app.py
@@ -78,8 +78,8 @@ class Incident(db.Model):
         self.response = None
 
 def send_email(to_email, subject, body):
-    sender_email = 'warzone20082003@gmail.com'
-    app_password = 'kdyxgzldaizlxydl'
+    sender_email = 'aditya.purushottam.kvs@gmail.com'
+    app_password = 'cxmubxhppngjferl'
 
     message = MIMEMultipart()
     message['From'] = sender_email
